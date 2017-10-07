2f7849a248d81a9189e0391eca42ae8c76095bcf
布小沫 <95112082@qq.com>
2017-10-08 03:02:14 +0800

    add sslocal

--- ssserver/docker-entrypoint.sh
+++ ssserver/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-[ $PASS = "**NULL**" ] && PASS=`pwgen -s 12 1`
+[ $PASSWORD = "**NULL**" ] && PASSWORD=`pwgen -s 12 1`
 echo "=> The Password is ${PASS}"
-ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASS} -m ${SERVER_METHOD} --fast-open
+ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD} --fast-open
