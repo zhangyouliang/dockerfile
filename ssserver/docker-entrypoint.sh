e48570db47155856a43929a6f138ab43232cbe86
布小沫 <95112082@qq.com>
2017-10-11 12:43:00 +0800

    update ssserver

--- ssserver/docker-entrypoint.sh
+++ ssserver/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 [ $PASSWORD = "**NULL**" ] && PASSWORD=`pwgen -s 12 1`
 echo "=> The Password is ${PASS}"
-ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD} --fast-open
+ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD}
