a333f31c41477226858b6498d15aea8cd66bef7b
布小沫 <95112082@qq.com>
2017-10-13 13:36:39 +0800

    更新脚本逻辑

--- gitblog/docker-entrypoint.sh
+++ gitblog/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if [ "$GIT_URL" != "**NULL**" ];then
     git clone ${GIT_URL} /www/blog
-    mv /www/blog/conf.yaml /www/conf.yaml
+    if [ -e /www/blog/conf.yaml ];then
+        mv /www/blog/conf.yaml /www/conf.yaml
+    else
+        mv /www/example-conf.yaml /www/conf.yaml
+    fi
 fi
 exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
