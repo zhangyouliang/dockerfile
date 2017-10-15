3dbf36dce9ddfd6593f04720bea2c736101f2ad6
布小沫 <95112082@qq.com>
2017-10-16 01:11:08 +0800

    Update docker-entrypoint.sh

--- ssserver/docker-entrypoint.sh
+++ ssserver/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 [ $PASSWORD = "**NULL**" ] && PASSWORD=`pwgen -s 12 1`
 echo "=> The Password is ${PASS}"
+echo "=> You can download the shadowsocks"
+echo "   Window:  http://ys-f.ys168.com/598741933/iTjHSWk4J536H265KM5/Shadowsocks.exe"
+echo "   Android: http://ys-n.ys168.com/598741957/jRgMVTn4J536H26KKM6/shadowsocks.apk"
+echo "   Mac:  http://ys-n.ys168.com/598741911/hSiJVfl3L452I6642OH/ShadowsocksX-NG.1.6.1.zip"
 ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD}
