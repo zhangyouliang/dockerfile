1461344aa0fa30675fdc5c68b6fea07bda58850c
布小沫 <95112082@qq.com>
2017-10-16 23:44:43 +0800

    完成增加用户授权，优化服务迁移

--- ngrok/README.md
+++ ngrok/README.md
@@ -1,4 +1,6 @@
 # ngrok
+
+## 使用默认授权
 ```
 docker run -d --name ngrok-server \
 -v /etc/ngrok:/etc/ngrok \
@@ -8,17 +10,32 @@ docker run -d --name ngrok-server \
 -p 3000-3010:3000-3010/tcp \
 -e NGROK_DOMAIN=daocloud.cc daocloud.io/buxiaomo/ngrok:1.7.1
 ```
+
+## 使用自定义授权
+```
+docker run -d --name ngrok-server \
+-v /etc/ngrok:/etc/ngrok \
+-p 80:80/tcp \
+-p 443:443/tcp \
+-p 4443:4443/tcp \
+-p 3000-3010:3000-3010/tcp \
+-e NGROK_DOMAIN=daocloud.cc \
+-e NGROK_USER=admin \
+-e NGROK_PASS=123456 \
+daocloud.io/buxiaomo/ngrok:1.7.1
+```
+
+
 # 迁移
 
-## 挂在了数据卷
+## 以挂载数据卷
 
 1、打包/etc/ngrok目录下所有文件，拷贝至新节点执行上面的命令即可
 
 2、更改域名解析
 
+## 未挂在数据卷
 
-## 没有挂在数据卷
-
-执行`docker inspect ngrok-server | jq '.[0].Mounts[0].Source'`命令获取卷，打包此目录中的所有文件，拷贝至新节点
+1、执行`docker inspect ngrok-server | jq '.[0].Mounts[0].Source'`命令获取卷，打包此目录中的所有文件，拷贝至新节点
 
 2、更改域名解析
--- ngrok/docker-entrypoint.sh
+++ ngrok/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 function StandardOutput {
     echo -e "\033[32m$1\033[0m"
 }
@@ -19,7 +19,7 @@ if [ ! -e ${NGROK_CONFIG}/install.lock ];then
         /etc/ngrok/package/arm \
         /etc/ngrok/package/mac64 \
         /etc/ngrok/package/mac32 \
-        ${NGROK_CONFIG}/tls \
+        ${NGROK_CONFIG}/tls
     cd ${NGROK_CONFIG}/tls
     openssl genrsa -out rootCA.key 2048 &> /dev/null
     openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem &> /dev/null
@@ -46,52 +46,56 @@ if [ ! -e ${NGROK_CONFIG}/install.lock ];then
     GOOS=darwin GOARCH=386 make release-client &> /dev/null
     GOOS=darwin GOARCH=amd64 make release-client &> /dev/null
 
-    cat > ngrok.cfg << EOF
+    cat > ngrok.yml << EOF
 server_addr: "$NGROK_DOMAIN:4443"
 trust_host_root_certs: false
 
 tunnels:
-  ssh:
-     remote_port: 22
-     proto:
-       tcp: "127.0.0.1:22"
-  mstsc:
-      remote_port: 3389
-      proto:
-       tcp: "127.0.0.1:3389"
-  web:
-   subdomain: "www"
-   proto:
-     http: 80
-  domain:
-   hostname: "www.example.com"
-   proto:
-     http: 80
+    ssh:
+        remote_port: 22
+        proto:
+            tcp: "127.0.0.1:22"
+    web:
+        subdomain: "www"
+        proto:
+            http: 80
+    auth:
+        auth: "admin:admin"
+        subdomain: "www"
+        proto:
+            http: 8080
+    domain:
+        hostname: "www.example.com"
+        proto:
+            http: 192.168.1.100:80
+    authdomain:
+        auth: "admin:admin"
+        hostname: "www.example.com"
+        proto:
+            http: 80
 EOF
     cat > start.bat << EOF
 @echo off
 ipconfig /flushdns
-ngrok.exe -config ngrok.cfg start ssh mstsc web
+ngrok.exe -authtoken "${NGROK_USER}:${NGROK_PAAS}" -config ngrok.cfg start ssh mstsc web
 pause
 EOF
     cat > start.sh << EOF
-./ngrok -config \$1 start `echo \$2 | sed 's/,/ /g'`
+./ngrok -authtoken "${NGROK_USER}:${NGROK_PAAS}" -config \$1 start \`\$2 | sed 's/,/ /g'\`
 EOF
     chmod +x start.sh
 
     StandardOutput "==> Package required files"
-    # mkdir package/linux64 package/linux32 package/win64 package/win32 package/arm package/mac64 package/mac32
+    cp -p ./start.bat ./ngrok.yml bin/windows_amd64/ngrok.exe ${NGROK_CONFIG}/package/win64/
+    cp -p ./start.bat ./ngrok.yml bin/windows_386/ngrok.exe ${NGROK_CONFIG}/package/win32/
 
-    cp -p ./start.bat ./ngrok.cfg bin/windows_amd64/ngrok.exe ${NGROK_CONFIG}/package/win64/
-    cp -p ./start.bat ./ngrok.cfg bin/windows_386/ngrok.exe ${NGROK_CONFIG}/package/win32/
+    cp -p ./start.bat ./ngrok.yml bin/linux_arm/ngrok ${NGROK_CONFIG}/package/arm/
 
-    cp -p ./start.bat ./ngrok.cfg bin/linux_arm/ngrok ${NGROK_CONFIG}/package/arm/
+    cp -p ./start.sh ./ngrok.yml bin/darwin_amd64/ngrok   ${NGROK_CONFIG}/package/mac64/
+    cp -p ./start.sh ./ngrok.yml bin/darwin_386/ngrok   ${NGROK_CONFIG}/package/mac32/
 
-    cp -p ./start.sh ./ngrok.cfg bin/darwin_amd64/ngrok   ${NGROK_CONFIG}/package/mac64/
-    cp -p ./start.sh ./ngrok.cfg bin/darwin_386/ngrok   ${NGROK_CONFIG}/package/mac32/
-
-    cp ./start.sh ./ngrok.cfg bin/ngrok ${NGROK_CONFIG}/package/linux64/
-    cp ./start.sh ./ngrok.cfg bin/linux_386/ngrok   ${NGROK_CONFIG}/package/linux32/
+    cp ./start.sh ./ngrok.yml bin/ngrok ${NGROK_CONFIG}/package/linux64/
+    cp ./start.sh ./ngrok.yml bin/linux_386/ngrok   ${NGROK_CONFIG}/package/linux32/
 
     cd ${NGROK_CONFIG}/package
     rm -rf *.zip
@@ -102,6 +106,7 @@ EOF
     	rm -rf ngrok
     done
     mv ${NGROK_HOME}/bin/ngrokd /etc/ngrok/ngrokd
+    echo "${NGROK_USER} ${NGROK_PAAS}" > ${NGROK_CONFIG}/secret
     [ $? == 0 ] && touch ${NGROK_CONFIG}/install.lock || exit 1
 fi
 
@@ -110,4 +115,4 @@ StandardOutput "----------------------------------------"
 StandardOutput "you can run command to see package dir"
 StandardOutput "  docker inspect $HOSTNAME | jq '.[0].Mounts[0].Source'"
 StandardOutput "----------------------------------------"
-exec /etc/ngrok/ngrokd -domain="$NGROK_DOMAIN" -tlsKey=${NGROK_CONFIG}/tls/snakeoil.key -tlsCrt=${NGROK_CONFIG}/tls/snakeoil.crt
+exec /etc/ngrok/ngrokd -secretPath ${NGROK_CONFIG}/secret -domain="$NGROK_DOMAIN" -tlsKey=${NGROK_CONFIG}/tls/snakeoil.key -tlsCrt=${NGROK_CONFIG}/tls/snakeoil.crt
