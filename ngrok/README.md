d84354051da781abb8be79bbe352d98815e8757c
布小沫 <95112082@qq.com>
2017-10-16 20:05:44 +0800

    优化迁移

--- ngrok/README.md
+++ ngrok/README.md
@@ -1,9 +1,24 @@
 # ngrok
 ```
 docker run -d --name ngrok-server \
+-v /etc/ngrok:/etc/ngrok \
 -p 80:80/tcp \
 -p 443:443/tcp \
 -p 4443:4443/tcp \
 -p 3000-3010:3000-3010/tcp \
 -e NGROK_DOMAIN=daocloud.cc daocloud.io/buxiaomo/ngrok:1.7.1
 ```
+# 迁移
+
+## 挂在了数据卷
+
+1、打包/etc/ngrok目录下所有文件，拷贝至新节点执行上面的命令即可
+
+2、更改域名解析
+
+
+## 没有挂在数据卷
+
+执行`docker inspect ngrok-server | jq '.[0].Mounts[0].Source'`命令获取卷，打包此目录中的所有文件，拷贝至新节点
+
+2、更改域名解析
--- ngrok/docker-entrypoint.sh
+++ ngrok/docker-entrypoint.sh
@@ -9,18 +9,30 @@ if [ ${NGROK_DOMAIN} == "**NULL**" ];then
     ErrorOutput "Please set the NGROK_DOMAIN environment variable !"
     exit 1
 fi
-if [ ! -e /usr/local/ngrok/package/install.lock ];then
+if [ ! -e ${NGROK_CONFIG}/install.lock ];then
     StandardOutput "==> Production certificate"
-    mkdir openssl && cd openssl
+    rm -rf /etc/ngrok/*
+    mkdir -p /etc/ngrok/package/linux64 \
+        /etc/ngrok/package/linux32 \
+        /etc/ngrok/package/win64 \
+        /etc/ngrok/package/win32 \
+        /etc/ngrok/package/arm \
+        /etc/ngrok/package/mac64 \
+        /etc/ngrok/package/mac32 \
+        ${NGROK_CONFIG}/tls \
+    cd ${NGROK_CONFIG}/tls
     openssl genrsa -out rootCA.key 2048 &> /dev/null
     openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem &> /dev/null
     openssl genrsa -out device.key 2048 &> /dev/null
     openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr &> /dev/null
     openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000 &> /dev/null
-    cp -f rootCA.pem ../assets/client/tls/ngrokroot.crt
-    cp -f device.crt ../assets/server/tls/snakeoil.crt
-    cp -f device.key ../assets/server/tls/snakeoil.key
-    cd ..
+    cp -f rootCA.pem ${NGROK_HOME}/assets/client/tls/ngrokroot.crt
+    mv rootCA.pem ngrokroot.crt
+    cp -f device.crt ${NGROK_HOME}/assets/server/tls/snakeoil.crt
+    mv device.crt snakeoil.crt
+    cp -f device.key ${NGROK_HOME}/assets/server/tls/snakeoil.key
+    mv device.key snakeoil.key
+    cd ${NGROK_HOME}
     StandardOutput "==> Compile Ngrok Server..."
     GOOS=linux GOARCH=amd64 make release-server &> /dev/null
     StandardOutput "==> Compile Ngrok Client For Linux ..."
@@ -68,20 +80,20 @@ EOF
     chmod +x start.sh
 
     StandardOutput "==> Package required files"
-    mkdir -p package/linux64 package/linux32 package/win64 package/win32 package/arm package/mac64 package/mac32
+    # mkdir package/linux64 package/linux32 package/win64 package/win32 package/arm package/mac64 package/mac32
 
-    cp -p {start.bat,ngrok.cfg,bin/windows_amd64/ngrok.exe} package/win64/
-    cp -p {start.bat,ngrok.cfg,bin/windows_386/ngrok.exe} package/win32/
+    cp -p ./start.bat ./ngrok.cfg bin/windows_amd64/ngrok.exe ${NGROK_CONFIG}/package/win64/
+    cp -p ./start.bat ./ngrok.cfg bin/windows_386/ngrok.exe ${NGROK_CONFIG}/package/win32/
 
-    cp -p {start.bat,ngrok.cfg,bin/linux_arm/ngrok} package/arm/
+    cp -p ./start.bat ./ngrok.cfg bin/linux_arm/ngrok ${NGROK_CONFIG}/package/arm/
 
-    cp -p {start.sh,ngrok.cfg,bin/darwin_amd64/ngrok}   package/mac64/
-    cp -p {start.sh,ngrok.cfg,bin/darwin_386/ngrok}   package/mac32/
+    cp -p ./start.sh ./ngrok.cfg bin/darwin_amd64/ngrok   ${NGROK_CONFIG}/package/mac64/
+    cp -p ./start.sh ./ngrok.cfg bin/darwin_386/ngrok   ${NGROK_CONFIG}/package/mac32/
 
-    cp {start.sh,ngrok.cfg,bin/ngrok}   package/linux64/
-    cp {start.sh,ngrok.cfg,bin/linux_386/ngrok}   package/linux32/
+    cp ./start.sh ./ngrok.cfg bin/ngrok ${NGROK_CONFIG}/package/linux64/
+    cp ./start.sh ./ngrok.cfg bin/linux_386/ngrok   ${NGROK_CONFIG}/package/linux32/
 
-    cd package
+    cd ${NGROK_CONFIG}/package
     rm -rf *.zip
     for p in `ls`
     do
@@ -89,7 +101,8 @@ EOF
     	zip ${p}.zip ngrok/* &> /dev/null
     	rm -rf ngrok
     done
-    [ $? == 0 ] && touch /usr/local/ngrok/package/install.lock | exit 1
+    mv ${NGROK_HOME}/bin/ngrokd /etc/ngrok/ngrokd
+    [ $? == 0 ] && touch ${NGROK_CONFIG}/install.lock || exit 1
 fi
 
 StandardOutput "==> Running ..."
@@ -97,4 +110,4 @@ StandardOutput "----------------------------------------"
 StandardOutput "you can run command to see package dir"
 StandardOutput "  docker inspect $HOSTNAME | jq '.[0].Mounts[0].Source'"
 StandardOutput "----------------------------------------"
-exec /usr/local/ngrok/bin/ngrokd -domain="$NGROK_DOMAIN" -tlsKey=/usr/local/ngrok/assets/server/tls/snakeoil.key -tlsCrt=/usr/local/ngrok/assets/server/tls/snakeoil.crt
+exec /etc/ngrok/ngrokd -domain="$NGROK_DOMAIN" -tlsKey=${NGROK_CONFIG}/tls/snakeoil.key -tlsCrt=${NGROK_CONFIG}/tls/snakeoil.crt
