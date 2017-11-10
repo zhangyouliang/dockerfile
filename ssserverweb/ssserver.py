846c1de496a6c9b571a40c5d0fcc708d0e357ba4
布小沫 <95112082@qq.com>
2017-11-11 04:21:16 +0800

    no message

--- ssserverweb/ssserver.py
+++ ssserverweb/ssserver.py
@@ -131,4 +131,4 @@ if __name__ == '__main__':
     DomainName = os.environ.get("DomainName")
 
     Task = {}
-    app.run(debug=True, host="127.0.0.1", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
