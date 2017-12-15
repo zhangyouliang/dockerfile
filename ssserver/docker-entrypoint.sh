dd8c3063e42cec225de848e9f624f4ca1c911ab4
buxiaomo <95112082@qq.com>
2017-12-15 14:55:27 +0800

    no message

--- ssserver/docker-entrypoint.sh
+++ ssserver/docker-entrypoint.sh
@@ -6,8 +6,9 @@ R=$(echo "${K} >= 3.7" | bc)
 if [ ${R} -eq 1 ];then
     OPS=" --fast-open --workers 20"
 
-else：
+else
     OPS=" --workers 20"
+fi
 echo "=> The Password is ${PASSWORD}"
 echo "=> You can download the shadowsocks"
 echo "   Window:  http://ys-f.ys168.com/598741933/iTjHSWk4J536H265KM5/Shadowsocks.exe"
