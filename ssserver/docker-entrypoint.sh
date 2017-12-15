b23bafcacd719f46a3be091cd42793910ef596eb
buxiaomo <95112082@qq.com>
2017-12-15 14:46:39 +0800

    更新fast-open以及workers参数

--- ssserver/docker-entrypoint.sh
+++ ssserver/docker-entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 [ $PASSWORD = "**NULL**" ] && PASSWORD=`pwgen -s 12 1`
+K=$(uname -r)
+K=${K:0:3}
+R=$(echo "${K} >= 3.7" | bc)
+if [ ${R} -eq 1 ];then
+    OPS=" --fast-open --workers 20"
+
+else：
+    OPS=" --workers 20"
 echo "=> The Password is ${PASSWORD}"
 echo "=> You can download the shadowsocks"
 echo "   Window:  http://ys-f.ys168.com/598741933/iTjHSWk4J536H265KM5/Shadowsocks.exe"
 echo "   Android: http://ys-n.ys168.com/598741957/jRgMVTn4J536H26KKM6/shadowsocks.apk"
 echo "   Mac:  http://ys-n.ys168.com/598741911/hSiJVfl3L452I6642OH/ShadowsocksX-NG.1.6.1.zip"
-ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD}
+ssserver -s ${SERVER_ADDR} -p 8388 -k ${PASSWORD} -m ${SERVER_METHOD} ${OPS}
