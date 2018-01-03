eae0cd14cc1d641d21c314c9015d51d3f6c8789d
buxiaomo <95112082@qq.com>
2018-01-03 10:49:12 +0800

    更名

--- splunk/build.sh
+++
@@ -1,9 +0,0 @@
-#https://docs.docker.com/engine/reference/commandline/build/
-if [ -z $CURRENT ]; then
-	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-fi
-
-docker build --no-cache=true -t splunk/splunk:7.0.0 .
-docker tag splunk/splunk:7.0.0 splunk/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:7.0.0 
