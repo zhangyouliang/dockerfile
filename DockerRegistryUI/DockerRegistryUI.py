5b36ff374936e685a5cd230b07e0de138dd5bd83
buxiaomo <95112082@qq.com>
2018-01-24 18:49:37 +0800

    增加nginx代理配置文件

--- DockerRegistryUI/DockerRegistryUI.py
+++ DockerRegistryUI/DockerRegistryUI.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template
-import requests,json,os
+import requests,json,os,sys
 
 app = Flask(__name__)
 
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def index():
     repositories = []
     namespace = {}
-    res = requests.get("http://%s/v2/_catalog" % (URL))
+    res = requests.get("http://%s/v2/_catalog" % (RegistryURL))
     for repo in json.loads(res.text)['repositories']:
         r = repo.split('/')
         t_repositories = {}
         t_repositories['name'] = r[1]
         t_repositories['namespace'] = r[0]
-        t_repositories['addr'] = URL + "/" + repo
+        t_repositories['addr'] = RegistryURL + "/" + repo
         repositories.append(t_repositories)
         if not namespace.has_key(str(r[0])):
             namespace[r[0]] = 0
@@ -32,10 +32,13 @@ def namespace(namespace):
 
 @app.route('/i/<namespace>/<name>')
 def imageinfo(namespace,name):
-    res = requests.get("http://%s/v2/%s/tags/list" % (URL,namespace+"/"+name))
+    res = requests.get("http://%s/v2/%s/tags/list" % (RegistryURL,namespace+"/"+name))
     # json.loads()
     return res.text
 
 if __name__ == '__main__':
-    URL = os.environ.get("RegistryURL")
+    RegistryURL = os.environ.get("RegistryURL")
+    if RegistryURL is None:
+        print("Please set RegistryURL !")
+        sys.exit()
     app.run(host="0.0.0.0", port=5000)
