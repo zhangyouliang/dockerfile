28eb63e4d6b1f0fb00bf6e43206983979e096915
buxiaomo <95112082@qq.com>
2018-01-27 00:25:37 +0800

    no message

--- redis/docker-entrypoint.sh
+++ redis/docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [[ ${GIT_URL} != "NULL" ]];then
+    git clone ${GIT_URL} /configure
+    cp /configure/${Configure_SCR} ${Configure_DEST}
+fi
+
 # first arg is `-f` or `--some-option`
 # or first arg is `something.conf`
 if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
@@ -10,7 +15,7 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	chown -R redis .
-	exec gosu redis "$0" "$@"
+	exec su-exec redis "$0" "$@"
 fi
 
 exec "$@"
--- redis/sentinel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -i "s/redis_master/${MASTER_HOSTNAME}/g" /etc/redis/sentinel.conf
-exec redis-sentinel /etc/redis/sentinel.conf --sentinel
