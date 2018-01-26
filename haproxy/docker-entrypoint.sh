a74134f54f413cfc24c513048fcfda1eaf59a258
buxiaomo <95112082@qq.com>
2018-01-27 00:11:30 +0800

    no message

---
+++ haproxy/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+if [[ ${GIT_URL} != "NULL" ]];then
+    git clone ${GIT_URL} /configure
+    cp /configure/${Configure_SCR} ${Configure_DEST}
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- haproxy "$@"
+fi
+
+if [ "$1" = 'haproxy' ]; then
+	shift # "haproxy"
+	# if the user wants "haproxy", let's add a couple useful flags
+	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
+	#   -db -- disables background mode
+	set -- haproxy -W -db "$@"
+fi
+
+exec "$@"
