e09481ad822c8800df439375688a5566e8b86b68
buxiaomo <95112082@qq.com>
2018-01-29 16:57:13 +0800

    no message

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+set -x
 if [[ ${GIT_URL} != "NULL" ]];then
     git clone ${GIT_URL} /configure
-    cp /configure/${Configure_SCR} ${Configure_DEST}
+    for n in $(seq 30);do
+        if [[ -n $(eval echo \$\{Configure_File_SCR_${n}\}) ]]; then
+            cp /configure/$(eval echo \$\{Configure_File_SCR_${n}\}) $(eval echo \$\{Configure_File_DEST_${n}\})
+        else
+            break
+        fi
+    done
 fi
 exec nginx -g "daemon off;"
