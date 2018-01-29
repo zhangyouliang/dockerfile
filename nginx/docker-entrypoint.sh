d97ca1088d22df94bd64cab35f6802a243255398
buxiaomo <95112082@qq.com>
2018-01-29 22:13:08 +0800

    no message

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,7 +3,7 @@ if [[ ${GIT_URL} != "NULL" ]];then
     git clone ${GIT_URL} /configure
     for n in $(seq 30);do
         if [[ -n $(eval echo \$\{Configure_File_SCR_${n}\}) ]]; then
-            mv /configure/$(eval echo \$\{Configure_File_SCR_${n}\}) $(eval echo \$\{Configure_File_DEST_${n}\})
+            cp -r /configure/$(eval echo \$\{Configure_File_SCR_${n}\}) $(eval echo \$\{Configure_File_DEST_${n}\})
         else
             break
         fi
