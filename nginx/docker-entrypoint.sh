646defb90236a418f81f3f76530d918b9fbef0c2
buxiaomo <95112082@qq.com>
2018-01-30 10:26:05 +0800

    修改shell逻辑

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -3,6 +3,9 @@ if [[ ${GIT_URL} != "NULL" ]];then
     git clone ${GIT_URL} /configure
     for n in $(seq 30);do
         if [[ -n $(eval echo \$\{Configure_File_SCR_${n}\}) ]]; then
+            if [[ -e $(eval echo \$\{Configure_File_DEST_${n}\}) ]]; then
+                rm -rf $(eval echo \$\{Configure_File_DEST_${n}\})
+            fi
             cp -r /configure/$(eval echo \$\{Configure_File_SCR_${n}\}) $(eval echo \$\{Configure_File_DEST_${n}\})
         else
             break
