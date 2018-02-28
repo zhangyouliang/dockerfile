b6a252cb2f9d9ed81cf6dbacc941c04622fbed5a
h422537 <h422537@163.com>
2018-02-28 18:33:02 +0800

    3.2.9

--- etcd/docker-entrypoint.sh
+++ etcd/docker-entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -e
-cat > /etc/etcd.conf < EOF
-# [member]
-ETCD_NAME="${ETCD_NAME}"
-ETCD_DATA_DIR="/etcd"
-ETCD_LISTEN_PEER_URLS="${ETCD_LISTEN_PEER_URLS}"
-ETCD_LISTEN_CLIENT_URLS="${ETCD_LISTEN_CLIENT_URLS}"
+# Check for $CLIENT_URLS
+if [ -z ${CLIENT_URLS+x} ]; then
+        CLIENT_URLS="http://0.0.0.0:4001,http://0.0.0.0:2379"
+        echo "Using default CLIENT_URLS ($CLIENT_URLS)"
+else
+        echo "Detected new CLIENT_URLS value of $CLIENT_URLS"
+fi
 
-#[cluster]
-ETCD_INITIAL_ADVERTISE_PEER_URLS="${ETCD_INITIAL_ADVERTISE_PEER_URLS}"
-ETCD_INITIAL_CLUSTER_STATE="${ETCD_INITIAL_CLUSTER_STATE}"
-ETCD_INITIAL_CLUSTER_TOKEN="${ETCD_INITIAL_CLUSTER_TOKEN}"
-ETCD_ADVERTISE_CLIENT_URLS="${ETCD_ADVERTISE_CLIENT_URLS}"
+# Check for $PEER_URLS
+if [ -z ${PEER_URLS+x} ]; then
+        PEER_URLS="http://0.0.0.0:7001,http://0.0.0.0:2380"
+        echo "Using default PEER_URLS ($PEER_URLS)"
+else
+        echo "Detected new PEER_URLS value of $PEER_URLS"
+fi
 
-# ETCD_INITIAL_CLUSTER="etcd1=http://k8s_master_ip1,etcd2=http://k8s_master_ip2:2380"
-EOF
-echo /etc/etcd.conf
-exec etcd --config-file /etc/etcd.conf
+ETCD_CMD="/bin/etcd -data-dir=/etcd -listen-peer-urls=${PEER_URLS} -listen-client-urls=${CLIENT_URLS} $*"
+echo -e "Running '$ETCD_CMD'\nBEGIN ETCD OUTPUT\n"
+
+exec $ETCD_CMD
