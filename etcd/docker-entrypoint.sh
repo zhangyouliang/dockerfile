6bc7219356459a87e2962a8ab289a272304d6d9c
h422537 <h422537@163.com>
2018-02-28 18:05:16 +0800

    3.3.1

--- etcd/docker-entrypoint.sh
+++ etcd/docker-entrypoint.sh
@@ -15,5 +15,5 @@ ETCD_ADVERTISE_CLIENT_URLS="${ETCD_ADVERTISE_CLIENT_URLS}"
 
 # ETCD_INITIAL_CLUSTER="etcd1=http://k8s_master_ip1,etcd2=http://k8s_master_ip2:2380"
 EOF
-echo /etc/etcd.conf 
+echo /etc/etcd.conf
 exec etcd --config-file /etc/etcd.conf
