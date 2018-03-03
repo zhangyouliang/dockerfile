2fede23e1e6457e7c21001aa802666a97689bc84
h422537 <h422537@163.com>
2018-03-04 00:10:19 +0800

    no message

--- proftpd/docker-entrypoint.sh
+++ proftpd/docker-entrypoint.sh
@@ -13,6 +13,9 @@ SQLNamedQuery updatecount UPDATE "count=count+1, accessed=now() WHERE userid='%u
 SQLLog  STOR,DELE modified
 SQLNamedQuery modified UPDATE "modified=now() WHERE userid='%u'" ftpuser
 SqlLogFile /var/log/proftpd/sql.log
+SQLDefaultUID ${SQLDefaultUID}
+SQLDefaultGID ${SQLDefaultGID}
 EOF
 
+sed -i "s/#.MasqueradeAddress.*/MasqueradeAddress 175.25.184.203/g" /etc/proftpd/proftpd.conf
 exec proftpd -nc /etc/proftpd/proftpd.conf
