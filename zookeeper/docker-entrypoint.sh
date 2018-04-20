c2f11f1f788c001d59f7147de72d96ac4f4edfec
MoMo <95112082@qq.com>
2018-04-21 00:18:40 +0800

    修改基础镜像，修改脚本逻辑，待测试

--- zookeeper/docker-entrypoint.sh
+++ zookeeper/docker-entrypoint.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
-set -e
-if [[ "${ZOOKEEPER_CLUSTER}" != "NULL" ]];then
-    if [[ "${ZOOKEEPER_ID}" == "NULL" ]];then
-        echo "Please Set ZOOKEEPER_ID"
-        exit 1
+id=false
+cluster=false
+
+function SET_ZOOKEEPER_CONFIGURE() {
+    grep -w "#$1" ${ZOOKEEPER_HOME}/conf/zoo.cfg &> /dev/null && sed -i "s/#$1/$1/g" ${ZOOKEEPER_HOME}/conf/zoo.cfg
+    grep -w $1 ${ZOOKEEPER_HOME}/conf/zoo.cfg &> /dev/null
+    if [ $? -eq 0 ];then
+        sed -i "s#$1=.*#$1=$2#g" ${ZOOKEEPER_HOME}/conf/zoo.cfg
     else
-        if [ ! -e /var/lib/zookeeper/myid ];then
-            echo "${ZOOKEEPER_ID}" > /var/lib/zookeeper/myid
-        fi
+        echo "$1=$2" >> ${ZOOKEEPER_HOME}/conf/zoo.cfg
+    fi
+}
+
+# 设置zookeeper参数
+for C in ${ZOOKEEPER_CONFIG}
+do
+    K=$(echo ${C} | awk -F '=' '{print $1}')
+    V=$(echo ${C} | awk -F '=' '{print $2}')
+    case ${K} in
+        "id" )
+            if [ ! -e /var/lib/zookeeper/myid ];then
+                echo "${K}" > /var/lib/zookeeper/myid
+            fi
+            id=true
+        ;;
+        "zookeeper.cluster" )
+            if [ ! -e /var/lib/zookeeper/myid ];then
+                echo ${K} | sed "s/,/\n/g" >> ${ZOOKEEPER_HOME}/conf/zoo.cfg
+            fi
+            cluster=true
+        ;;
+        * )
+            if [ ${K} != "dataDir" ];then
+                SET_ZOOKEEPER_CONFIGURE ${K} ${V}
+            fi
+        ;;
+    esac
+done
+if [[ ${cluster} == true ]];then
+    if [[ ${id} == false ]];then
+        echo "Please Set id"
+        exit 1
     fi
 fi
-echo $ZOOKEEPER_CLUSTER | sed "s/,/\n/g" >> ${ZOOKEEPER_HOME}/conf/zoo.cfg
 
-exec bin/zkServer.sh start-foreground
+exec ${ZOOKEEPER_HOME}/bin/zkServer.sh start-foreground
