ea05cdf72f647633739d459dcb206909d625f08c
MoMo <95112082@qq.com>
2018-04-20 23:59:09 +0800

    修改启动脚本逻辑，固定kafka数据卷

--- kafka/docker-entrypoint.sh
+++ kafka/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function set_kafka_configure() {
+function SET_KAFKA_CONFIGURE() {
     grep -w "#$1" ${KAFKA_HOME}/config/server.properties &> /dev/null && sed -i "s/#$1/$1/g" ${KAFKA_HOME}/config/server.properties
     grep -w $1 ${KAFKA_HOME}/config/server.properties &> /dev/null
     if [ $? -eq 0 ];then
@@ -14,22 +14,21 @@ for C in ${KAFKA_CONFIG}
 do
     K=$(echo ${C} | awk -F '=' '{print $1}')
     V=$(echo ${C} | awk -F '=' '{print $2}')
-    # 设置zookeeper集群配置
-    if [ ${K} == "zookeeper.connect" ];then
-        if [ ${C} == "localhost:2181" ];then
-            ${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
-        else
-            set_kafka_configure zookeeper.connect ${ZOOKEEPER_CLUSTER}
-            ZK_IS_SET=true
+    if [ ${K} != "log.dirs" ];then
+        # 设置zookeeper集群配置
+        if [ ${K} == "zookeeper.connect" ];then
+            if [ ${C} == "localhost:2181" ];then
+                ${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
+            else
+                SET_KAFKA_CONFIGURE zookeeper.connect ${ZOOKEEPER_CLUSTER}
+                ZK_IS_SET=true
+            fi
         fi
+        if [ ${ZK_IS_SET} == false ];then
+            ${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
+        fi
+        SET_KAFKA_CONFIGURE ${K} ${V}
     fi
-    if [ ${ZK_IS_SET} == false ];then
-        ${KAFKA_HOME}/bin/zookeeper-server-start.sh ${KAFKA_HOME}/config/zookeeper.properties &
-    fi
-    set_kafka_configure ${K} ${V}
 done
-
-# 固定的Log日志存储目录，不允许修改
-set_kafka_configure log.dirs /var/lib/kafka
 
 exec ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties
