dc0d9f12c2fbee5a823b1dc23c3fcb062e2dae7b
MoMo <95112082@qq.com>
2018-05-17 01:47:12 +0800

    no message

--- proftpd/docker-entrypoint.sh
+++ proftpd/docker-entrypoint.sh
@@ -4,6 +4,6 @@ MasqueradeAddress=$(curl -s  --unix-socket /run/docker.sock http://unix/info  | 
 sed -i "s/^#.MasqueradeAddress.*/MasqueradeAddress ${MasqueradeAddress}/g" /etc/proftpd/proftpd.conf
 echo -e "\n\nSQLConnectInfo ${MYSQL_DATABASE}@${MYSQL_HOST}:${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD}" >> /etc/proftpd/sql.conf
 chown -R www-data:www-data /var/www
-chmod -R 775 /var/www
+# chmod -R 775 /var/www
 
 exec proftpd -nc /etc/proftpd/proftpd.conf
