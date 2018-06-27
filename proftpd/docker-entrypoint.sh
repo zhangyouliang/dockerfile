36ecb4ba7949b49fc7b979e32f27547b9149ea35
MoMo <95112082@qq.com>
2018-06-27 17:53:38 +0800

    允许设置FTP被动IP地址

--- proftpd/docker-entrypoint.sh
+++ proftpd/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-MasqueradeAddress=$(curl -s  --unix-socket /run/docker.sock http://unix/info  | jq -r .Swarm.NodeAddr)
+if [ ${MasqueradeAddress} == "**IP**" ];then
+    MasqueradeAddress=$(curl -s  --unix-socket /run/docker.sock http://unix/info  | jq -r .Swarm.NodeAddr)
+fi
 sed -i "s/^#.MasqueradeAddress.*/MasqueradeAddress ${MasqueradeAddress}/g" /etc/proftpd/proftpd.conf
 echo -e "\n\nSQLConnectInfo ${MYSQL_DATABASE}@${MYSQL_HOST}:${MYSQL_PORT} ${MYSQL_USER} ${MYSQL_PASSWORD}" >> /etc/proftpd/sql.conf
 chown -R 82:82 /var/www
@@ -16,5 +18,23 @@ chmod 755 /var/www
 # mysql --defaults-file=/tmp/mysql.cnf ${MYSQL_DATABASE} < /var/lib/proftpd/ftp_group.sql
 # mysql --defaults-file=/tmp/mysql.cnf ${MYSQL_DATABASE} < /var/lib/proftpd/ftp_user.sql
 # rm -rf /tmp/mysql.cnf
+
+if [ -d /etc/proftpd/ssl ];then
+    sed -i "s|#Include /etc/proftpd/tls.conf|Include /etc/proftpd/tls.conf|g" /etc/proftpd/proftpd.conf
+    cat > /etc/proftpd/tls.conf << EOF
+<IfModule mod_tls.c>
+    TLSEngine                  on
+    TLSLog                     /var/log/proftpd/tls.log
+    TLSProtocol TLSv1.2
+    TLSCipherSuite AES128+EECDH:AES128+EDH
+    TLSOptions                 NoCertRequest AllowClientRenegotiations
+    TLSRSACertificateFile      ${TSL_CERTFILE}
+    TLSRSACertificateKeyFile   ${TSL_KEYFILE}
+    TLSVerifyClient            off
+    TLSRequired                on
+    RequireValidShell          no
+</IfModule>
+EOF
+fi
 
 exec proftpd -nc /etc/proftpd/proftpd.conf
